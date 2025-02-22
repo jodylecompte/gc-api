0697542a8dd5744cc48c65da9bc95b62088157e8
Jody LeCompte <jody@jodylecompte.com>
2025-02-22 15:01:40 -0600

    Jk about the previous commit

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\QuestionController;
 
-Route::get('/questions/byId/{param}', [QuestionController::class, 'lookup'])
-    ->header('Access-Control-Allow-Origin', '*')
-    ->header('Access-Control-Allow-Methods', 'POST, GET')
-    ->header('Access-Control-Allow-Headers', 'Content-Type');
-Route::post('/questions/byUrl', [QuestionController::class, 'lookup'])
-    ->header('Access-Control-Allow-Origin', '*')
-    ->header('Access-Control-Allow-Methods', 'POST, GET')
-    ->header('Access-Control-Allow-Headers', 'Content-Type');
-;
+Route::get('/questions/byId/{param}', [QuestionController::class, 'lookup']);
+Route::post('/questions/byUrl', [QuestionController::class, 'lookup']);
